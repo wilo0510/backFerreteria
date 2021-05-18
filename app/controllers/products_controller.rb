d5bab2ea60f7314c53d35c626be3a2a5130a178f
wilmer <wilmer.pachon@puntopay.co>
2021-05-18 11:40:39 -0500

    cors policy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,18 @@
 class ProductsController < ApplicationController
+    rescue_from Exception do |e|
+        render json: {error: e.message}, status: :internal_server_error
+    end
+    rescue_from ActiveRecord::RecordNotFound do |e|
+        render json:{error: e.message},status: :not_found
+    end
+    rescue_from ActiveRecord::RecordInvalid do |e|
+        render json: {error: e.message}, status: :unprocessable_entity
+    end
     def search
-        render json: {api: 'Buscando producto'}, status: :ok 
+        products = Product.product_finder(           
+            product_name:       params[:product_name]            
+        )
+        render json: products, status: :ok 
     end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+    class<< self
+        def product_finder(product_name:)
+            products    = Product.all
+            unless product_name.nil?
+                products    = products.where("lower(nameproduct) LIKE ?", "%" + product_name.downcase + "%") if product_name != ""
+            end           
+            products
+        end
+    end
 end
